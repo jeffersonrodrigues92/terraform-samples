ecf4fa117cd8fb194538e9edb7f1cddcbbef8e66
Jefferson <jefferson.comff@hotmail.com>
2022-02-01 13:08:50 -0300

    first commit

---
+++ modules/ec2/ec2.tf
@@ -0,0 +1,18 @@
+variable "ec2_name" {
+    type = string
+}
+
+
+resource "aws_instance" "instance" {
+  ami = "ami-0a8b4cd432b1c3063"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name =  var.ec2_name
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.instance.id
+}
+
---
+++ modules/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_module" {
+  source = "./ec2"
+  ec2_name = "Name From Module" 
+}
+
+
+output "module_output" {
+  value = module.ec2_module.instance_id
+}
